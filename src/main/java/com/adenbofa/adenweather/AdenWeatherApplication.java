22cb288ff8287a990d3ba66a46a2d7b99300a1bf
Daerego Braide <phronesis4xt@gmail.com>
2024-04-17 16:19:14 +0100

    first commit

---
+++ src/main/java/com/adenbofa/adenweather/AdenWeatherApplication.java
@@ -0,0 +1,13 @@
+package com.adenbofa.adenweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdenWeatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdenWeatherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/adenbofa/adenweather/AdenWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adenbofa.adenweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdenWeatherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
